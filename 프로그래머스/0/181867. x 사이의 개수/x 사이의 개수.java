e692dee57ad864279df5cfd63951387abdfcb38c
jungheol <79863514+jungheol@users.noreply.github.com>
2024-03-16 19:48:33 +0900

    [level 0] Title: x 사이의 개수, Time: 2.30 ms, Memory: 83.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] solution(String myString) {
+        String[] tmp = myString.split("x", -1);
+        int[] answer = new int[tmp.length];
+        
+        for(int i = 0; i < tmp.length; i++) {
+            answer[i] = tmp[i].length();
+        }
+        return answer;
+    }
+}
