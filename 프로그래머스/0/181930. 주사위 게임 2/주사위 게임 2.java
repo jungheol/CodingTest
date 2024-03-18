f7aa4880710aa23628625aeae6f78e1a1a719c0a
jungheol <79863514+jungheol@users.noreply.github.com>
2024-03-18 15:20:25 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.01 ms, Memory: 79.8 MB -BaekjoonHub

--- 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.java
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.java
@@ -1,11 +1,9 @@
 class Solution {
     public int solution(int a, int b, int c) {
-        int answer = 0;
-        if (a==b && b==c && a==c) {
+        int answer = a+b+c;
+        if (a==b && b==c) {
             answer = (a+b+c) * (a*a + b*b + c*c) * (a*a*a + b*b*b + c*c*c);
-        } else if (a!=b && b!=c && a!=c) {
-            answer = a+b+c;
-        } else if ((a==b && b!=c) || (a!=b && b==c) || (a==c && a!=b)){
+        } else if ((a==b) || (b==c) || (a==c)){
             answer = (a+b+c) * (a*a + b*b + c*c);
         }
         return answer;
