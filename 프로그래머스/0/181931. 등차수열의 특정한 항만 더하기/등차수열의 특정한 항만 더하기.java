c8a506308589e4526d1ddd0abeab26d5cc381b63
jungheol <79863514+jungheol@users.noreply.github.com>
2024-03-18 14:57:01 +0900

    [level 0] Title: 등차수열의 특정한 항만 더하기, Time: 0.03 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int a, int d, boolean[] included) {
+        int answer = 0;
+        int[] arr = new int[included.length];
+        
+        for (int i = 0; i < included.length; i++) {
+            arr[i] = a + (d*i);
+            if(included[i]) answer += arr[i];
+        }
+        
+        return answer;
+    }
+}
