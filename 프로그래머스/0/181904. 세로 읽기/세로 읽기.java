ccd37ec4f14ed61ad1ff4672825c191c567fc824
jungheol <79863514+jungheol@users.noreply.github.com>
2024-03-19 19:12:40 +0900

    [level 0] Title: 세로 읽기, Time: 10.14 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/0/181904. 세로 읽기/세로 읽기.java
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.java
@@ -1,15 +1,9 @@
 class Solution {
     public String solution(String my_string, int m, int c) {
         String answer = "";
-        int num = my_string.length()/m;
-        String[][] strArr = new String[num][m];
-        int sum = 0;
-        for(int i = 0; i < strArr.length; i++) {
-            for(int j = 0; j < m; j++) {
-                strArr[i][j] = my_string.substring(sum, sum+1);
-                sum++;
-            }
-            answer += strArr[i][c-1];
+        
+        for(int i = c-1; i < my_string.length(); i += m) {
+            answer += my_string.charAt(i);
         }
         
         return answer;
