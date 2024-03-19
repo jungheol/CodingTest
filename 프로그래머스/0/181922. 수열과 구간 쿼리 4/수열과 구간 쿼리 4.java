d8454876d04bcd0d2658045966882c512fac0edb
jungheol <79863514+jungheol@users.noreply.github.com>
2024-03-19 18:10:53 +0900

    [level 0] Title: 수열과 구간 쿼리 4, Time: 2.66 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.java
+++ 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.java
@@ -1,13 +1,10 @@
 class Solution {
-    public int[] solution(int[] arr, int[][] queries) {
-        int[] answer = {};
-        
+    public int[] solution(int[] arr, int[][] queries) {        
         for(int i = 0; i < queries.length; i++) {
             for (int j = queries[i][0]; j <= queries[i][1]; j++){
                 if(j % queries[i][2] == 0) arr[j] += 1;
             }
         }
-        answer = arr;
-        return answer;
+        return arr;
     }
 }
