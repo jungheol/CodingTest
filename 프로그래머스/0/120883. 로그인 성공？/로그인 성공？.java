89dbd9138220edfc01316b57e0a7a169b61bd516
jungheol <79863514+jungheol@users.noreply.github.com>
2024-03-20 00:24:06 +0900

    [level 0] Title: 로그인 성공?, Time: 0.02 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.java
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.java
@@ -2,11 +2,14 @@ class Solution {
     public String solution(String[] id_pw, String[][] db) {
         String answer = "";
         for(int i = 0; i < db.length; i++) {
-            if(id_pw[0].equals(db[i][0]) && id_pw[1].equals(db[i][1])) {answer = "login"; break;}
-            else if(id_pw[0].equals(db[i][0]) && !id_pw[1].equals(db[i][1])) { answer = "wrong pw"; break;}
-            else answer = "fail";
-            
+            if(db[i][0].equals(id_pw[0])) {
+                if(db[i][1].equals(id_pw[1])) {
+                    return "login";
+                }
+                return "wrong pw";
+            }
         }
-        return answer;
+        return "fail";
+        
     }
 }
