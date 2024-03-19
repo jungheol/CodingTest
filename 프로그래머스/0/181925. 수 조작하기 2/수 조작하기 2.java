57fade87efa16bd8b6b3057377671f61f5219cba
jungheol <79863514+jungheol@users.noreply.github.com>
2024-03-19 15:37:56 +0900

    [level 0] Title: 수 조작하기 2, Time: 682.36 ms, Memory: 384 MB -BaekjoonHub

--- 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
@@ -2,20 +2,18 @@ import java.util.*;
 
 class Solution {
     public String solution(int[] numLog) {
-        StringBuilder answer = new StringBuilder();
+        String answer = "";
         
         for(int i = 1; i < numLog.length; i++) {
-            if (numLog[i] - numLog[i-1] == 1) {
-                answer.append("w");
-            } else if(numLog[i] - numLog[i-1] == -1) {
-                answer.append("s");
-            } else if(numLog[i] - numLog[i-1] == 10) {
-                answer.append("d");
-            } else if(numLog[i] - numLog[i-1] == -10) {
-                answer.append("a");
-            } 
+            int sum = numLog[i] - numLog[i-1];
+            switch(sum) {
+                case 1 : answer+="w"; break;
+                case -1 : answer+="s"; break;
+                case 10 : answer+="d"; break;
+                case -10 : answer+="a"; break;
+            }
         }
         
-        return answer.toString();
+        return answer;
     }
 }
