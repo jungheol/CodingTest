f6a2491864912ce85d82ed2cb6f160bfbd7d3223
jungheol <79863514+jungheol@users.noreply.github.com>
2024-03-21 15:31:34 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.02 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
@@ -1,10 +1,5 @@
 class Solution {
     public int solution(String my_string, String is_suffix) {
-        boolean isEnd = false;
-        
-        isEnd = my_string.endsWith(is_suffix);
-        
-        if(isEnd) return 1;
-        else return 0;
+        return my_string.endsWith(is_suffix) ? 1 : 0;
     }
 }
