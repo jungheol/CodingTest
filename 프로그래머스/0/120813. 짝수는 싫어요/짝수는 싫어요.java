a953ae472d72f3124eea7427e147ba26194d82bd
jungheol <79863514+jungheol@users.noreply.github.com>
2024-04-05 17:06:30 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.04 ms, Memory: 82.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Solution {
+    public ArrayList solution(int n) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
+            if(i % 2 != 0) list.add(i);
+        }
+        return list;
+    }
+}
