dee06da45b058ff958cc2317b18645ec27b958c7
jungheol <79863514+jungheol@users.noreply.github.com>
2024-04-11 21:32:38 +0900

    [level 1] Title: 폰켓몬, Time: 3.35 ms, Memory: 83.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        HashSet<Integer> set = new HashSet<>();
+        for(int i=0; i < nums.length; i++) {
+            set.add(nums[i]);
+        }
+    
+        return set.size() > nums.length / 2 ? nums.length / 2 : set.size();
+    }
+}
