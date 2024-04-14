b2a30738264997e4e571766d063e96b89ed2d5f7
jungheol <79863514+jungheol@users.noreply.github.com>
2024-04-15 00:05:47 +0900

    [level 1] Title: 덧칠하기, Time: 0.95 ms, Memory: 72.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int n, int m, int[] section) {
+        int answer = 0;
+        int start = section[0];
+        answer++;
+        
+        for(int i : section) {
+            if(start + m > i) continue;
+            
+            start = i;
+            answer++;
+        }
+        
+        return answer;
+    }
+}
