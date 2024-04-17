ab699247f7e4939019bf29c6a122b60dcb99e46d
jungheol <79863514+jungheol@users.noreply.github.com>
2024-04-17 12:57:15 +0900

    [Bronze III] Title: 최소, 최대, Time: 1696 ms, Memory: 318096 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -4,14 +4,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();    
-        sc.nextLine();
-        String[] st = sc.nextLine().split(" ");
+        int N = sc.nextInt();
         int[] num = new int[N];
         for (int i = 0; i < num.length; i++) {
-            num[i] = Integer.parseInt(st[i]);
+            num[i] = sc.nextInt();
         }
-        Arrays.sort(num);
-        System.out.println(num[0] + " " + num[N-1]);
+        int min = num[0];
+        int max = num[0];
+        for (int i = 1; i < num.length; i++) {
+            if(min > num[i]) min = num[i];
+            if(max < num[i]) max = num[i];
+        }
+        System.out.println(min + " " + max);
     }
 }
