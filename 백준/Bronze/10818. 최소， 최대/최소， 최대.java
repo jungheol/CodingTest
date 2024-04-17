995bb3ef809021f523d7b09acad74bb0bd59adab
jungheol <79863514+jungheol@users.noreply.github.com>
2024-04-17 12:59:32 +0900

    [Bronze III] Title: 최소, 최대, Time: 504 ms, Memory: 92140 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -1,13 +1,16 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         int[] num = new int[N];
         for (int i = 0; i < num.length; i++) {
-            num[i] = sc.nextInt();
+            num[i] = Integer.parseInt(st.nextToken());
         }
         int min = num[0];
         int max = num[0];
